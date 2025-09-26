ebd268d9b7d4567b3a9995c43efd5dba7614dfc4
Kartik <avaghankartik@gmail.com>
2025-09-26 13:52:03 +0530

    nav route is fixed

--- src/components/RCMembers/Nav.jsx
+++ src/components/RCMembers/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
     {
       name: 'Title Registration',
       icon: FileText,
-      href: '/registration',
+      href: '/ecdashboard',
       active: true
     },
     {
--- src/components/staff/Nav.jsx
+++ src/components/staff/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
     {
       name: 'Title Registration',
       icon: FileText,
-      href: '/registration',
+      href: '/staffdashboard',
       active: true
     },
     {
